a870f14b28ca441424969bf2cd8fcd0738668eb3
ClashSAN <98228077+ClashSAN@users.noreply.github.com>
2022-11-17 22:16:40 +0000

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
-xcopy /Y "file_utils.py" "venv\Lib\site-packages\transformers\file-utils.py"
 python -m venv venv
 CALL venv\Scripts\activate.bat
 pip install -r requirements.txt
+xcopy /Y "file_utils.py" "venv\Lib\site-packages\transformers\file-utils.py"
 START cmd
