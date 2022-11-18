ec5f754e8dc12fbd0828004e05b5928022c2ead3
ClashSAN <98228077+ClashSAN@users.noreply.github.com>
2022-11-18 07:32:36 +0000

    for linux make sure your system-wide python =3.9.X

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-source venv/Scripts/activate
+#make sure your python is 3.9.10 or 3.9.12 for linux. Those I've tested. 3.10 may have issues.
+python -m venv venv
+source venv/bin/activate
 pip install -r requirements.txt
 
 # launch terminal. substitute konsole with your preference
